01e93c9e451dd3f789ad946087d6333cf65f2d2d
Parisa <p.shekari1374@gmail.com>
2023-06-12 02:27:06 +0330

    details2

--- RestaurantChargoon.UI.WinForm/Forms/Users/UserInfoForm.Designer.cs
+++ RestaurantChargoon.UI.WinForm/Forms/Users/UserInfoForm.Designer.cs
@@ -28,176 +28,187 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			AddressTetxtBox = new TextBox();
-			PasswordTetxtBox = new TextBox();
-			NationalCodeTetxtBox = new TextBox();
-			LastNameTetxtBox = new TextBox();
-			NameTetxtBox = new TextBox();
-			RestaurantManagerCheckBox = new CheckBox();
-			label5 = new Label();
-			label4 = new Label();
-			label3 = new Label();
-			label2 = new Label();
-			label1 = new Label();
-			EditButton = new Button();
-			CancelButton = new Button();
-			SaveButton = new Button();
-			SuspendLayout();
-			// 
-			// AddressTetxtBox
-			// 
-			AddressTetxtBox.Location = new Point(51, 288);
-			AddressTetxtBox.Name = "AddressTetxtBox";
-			AddressTetxtBox.RightToLeft = RightToLeft.Yes;
-			AddressTetxtBox.Size = new Size(242, 27);
-			AddressTetxtBox.TabIndex = 23;
-			// 
-			// PasswordTetxtBox
-			// 
-			PasswordTetxtBox.Location = new Point(51, 225);
-			PasswordTetxtBox.Name = "PasswordTetxtBox";
-			PasswordTetxtBox.PasswordChar = '*';
-			PasswordTetxtBox.Size = new Size(242, 27);
-			PasswordTetxtBox.TabIndex = 22;
-			// 
-			// NationalCodeTetxtBox
-			// 
-			NationalCodeTetxtBox.Location = new Point(51, 149);
-			NationalCodeTetxtBox.Name = "NationalCodeTetxtBox";
-			NationalCodeTetxtBox.Size = new Size(242, 27);
-			NationalCodeTetxtBox.TabIndex = 21;
-			// 
-			// LastNameTetxtBox
-			// 
-			LastNameTetxtBox.Location = new Point(51, 81);
-			LastNameTetxtBox.Name = "LastNameTetxtBox";
-			LastNameTetxtBox.RightToLeft = RightToLeft.Yes;
-			LastNameTetxtBox.Size = new Size(242, 27);
-			LastNameTetxtBox.TabIndex = 20;
-			// 
-			// NameTetxtBox
-			// 
-			NameTetxtBox.Location = new Point(51, 18);
-			NameTetxtBox.Name = "NameTetxtBox";
-			NameTetxtBox.RightToLeft = RightToLeft.Yes;
-			NameTetxtBox.Size = new Size(242, 27);
-			NameTetxtBox.TabIndex = 19;
-			// 
-			// RestaurantManagerCheckBox
-			// 
-			RestaurantManagerCheckBox.AutoSize = true;
-			RestaurantManagerCheckBox.Enabled = false;
-			RestaurantManagerCheckBox.Location = new Point(179, 350);
-			RestaurantManagerCheckBox.Name = "RestaurantManagerCheckBox";
-			RestaurantManagerCheckBox.RightToLeft = RightToLeft.Yes;
-			RestaurantManagerCheckBox.Size = new Size(113, 24);
-			RestaurantManagerCheckBox.TabIndex = 18;
-			RestaurantManagerCheckBox.Text = "مدیر رستوران";
-			RestaurantManagerCheckBox.UseVisualStyleBackColor = true;
-			// 
-			// label5
-			// 
-			label5.AutoSize = true;
-			label5.Location = new Point(317, 288);
-			label5.Name = "label5";
-			label5.Size = new Size(44, 20);
-			label5.TabIndex = 17;
-			label5.Text = "آدرس";
-			// 
-			// label4
-			// 
-			label4.AutoSize = true;
-			label4.Location = new Point(317, 225);
-			label4.Name = "label4";
-			label4.Size = new Size(48, 20);
-			label4.TabIndex = 16;
-			label4.Text = "پسورد";
-			// 
-			// label3
-			// 
-			label3.AutoSize = true;
-			label3.Location = new Point(317, 156);
-			label3.Name = "label3";
-			label3.Size = new Size(56, 20);
-			label3.TabIndex = 15;
-			label3.Text = "کد ملی";
-			// 
-			// label2
-			// 
-			label2.AutoSize = true;
-			label2.Location = new Point(317, 81);
-			label2.Name = "label2";
-			label2.Size = new Size(90, 20);
-			label2.TabIndex = 14;
-			label2.Text = "نام خانوادگی";
-			// 
-			// label1
-			// 
-			label1.AutoSize = true;
-			label1.Location = new Point(317, 18);
-			label1.Name = "label1";
-			label1.Size = new Size(27, 20);
-			label1.TabIndex = 13;
-			label1.Text = "نام";
-			// 
-			// EditButton
-			// 
-			EditButton.Location = new Point(293, 404);
-			EditButton.Name = "EditButton";
-			EditButton.Size = new Size(94, 29);
-			EditButton.TabIndex = 24;
-			EditButton.Text = "ویرایش";
-			EditButton.UseVisualStyleBackColor = true;
-			EditButton.Click += EditButton_Click;
-			// 
-			// CancelButton
-			// 
-			CancelButton.Location = new Point(165, 404);
-			CancelButton.Name = "CancelButton";
-			CancelButton.Size = new Size(94, 29);
-			CancelButton.TabIndex = 25;
-			CancelButton.Text = "انصراف";
-			CancelButton.UseVisualStyleBackColor = true;
-			CancelButton.Click += CancelButton_Click;
-			// 
-			// SaveButton
-			// 
-			SaveButton.Location = new Point(37, 404);
-			SaveButton.Name = "SaveButton";
-			SaveButton.Size = new Size(94, 29);
-			SaveButton.TabIndex = 26;
-			SaveButton.Text = "ذخیره";
-			SaveButton.UseVisualStyleBackColor = true;
-			SaveButton.Click += SaveButton_Click;
-			// 
-			// UserInfoForm
-			// 
-			AutoScaleDimensions = new SizeF(8F, 20F);
-			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(425, 472);
-			Controls.Add(SaveButton);
-			Controls.Add(CancelButton);
-			Controls.Add(EditButton);
-			Controls.Add(AddressTetxtBox);
-			Controls.Add(PasswordTetxtBox);
-			Controls.Add(NationalCodeTetxtBox);
-			Controls.Add(LastNameTetxtBox);
-			Controls.Add(NameTetxtBox);
-			Controls.Add(RestaurantManagerCheckBox);
-			Controls.Add(label5);
-			Controls.Add(label4);
-			Controls.Add(label3);
-			Controls.Add(label2);
-			Controls.Add(label1);
-			MaximumSize = new Size(443, 519);
-			MinimumSize = new Size(443, 519);
-			Name = "UserInfoForm";
-			StartPosition = FormStartPosition.CenterScreen;
-			Text = "اطلاعات کاربر";
-			FormClosed += UserInfoForm_FormClosed;
-			Load += UserInfoForm_Load;
-			ResumeLayout(false);
-			PerformLayout();
+            this.AddressTetxtBox = new System.Windows.Forms.TextBox();
+            this.PasswordTetxtBox = new System.Windows.Forms.TextBox();
+            this.NationalCodeTetxtBox = new System.Windows.Forms.TextBox();
+            this.LastNameTetxtBox = new System.Windows.Forms.TextBox();
+            this.NameTetxtBox = new System.Windows.Forms.TextBox();
+            this.RestaurantManagerCheckBox = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddressTetxtBox
+            // 
+            this.AddressTetxtBox.Location = new System.Drawing.Point(45, 216);
+            this.AddressTetxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.AddressTetxtBox.Name = "AddressTetxtBox";
+            this.AddressTetxtBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.AddressTetxtBox.Size = new System.Drawing.Size(212, 23);
+            this.AddressTetxtBox.TabIndex = 23;
+            // 
+            // PasswordTetxtBox
+            // 
+            this.PasswordTetxtBox.Location = new System.Drawing.Point(45, 169);
+            this.PasswordTetxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PasswordTetxtBox.Name = "PasswordTetxtBox";
+            this.PasswordTetxtBox.PasswordChar = '*';
+            this.PasswordTetxtBox.Size = new System.Drawing.Size(212, 23);
+            this.PasswordTetxtBox.TabIndex = 22;
+            // 
+            // NationalCodeTetxtBox
+            // 
+            this.NationalCodeTetxtBox.Location = new System.Drawing.Point(45, 112);
+            this.NationalCodeTetxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.NationalCodeTetxtBox.Name = "NationalCodeTetxtBox";
+            this.NationalCodeTetxtBox.Size = new System.Drawing.Size(212, 23);
+            this.NationalCodeTetxtBox.TabIndex = 21;
+            // 
+            // LastNameTetxtBox
+            // 
+            this.LastNameTetxtBox.Location = new System.Drawing.Point(45, 61);
+            this.LastNameTetxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LastNameTetxtBox.Name = "LastNameTetxtBox";
+            this.LastNameTetxtBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.LastNameTetxtBox.Size = new System.Drawing.Size(212, 23);
+            this.LastNameTetxtBox.TabIndex = 20;
+            // 
+            // NameTetxtBox
+            // 
+            this.NameTetxtBox.Location = new System.Drawing.Point(45, 14);
+            this.NameTetxtBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.NameTetxtBox.Name = "NameTetxtBox";
+            this.NameTetxtBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.NameTetxtBox.Size = new System.Drawing.Size(212, 23);
+            this.NameTetxtBox.TabIndex = 19;
+            // 
+            // RestaurantManagerCheckBox
+            // 
+            this.RestaurantManagerCheckBox.AutoSize = true;
+            this.RestaurantManagerCheckBox.Enabled = false;
+            this.RestaurantManagerCheckBox.Location = new System.Drawing.Point(157, 262);
+            this.RestaurantManagerCheckBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.RestaurantManagerCheckBox.Name = "RestaurantManagerCheckBox";
+            this.RestaurantManagerCheckBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RestaurantManagerCheckBox.Size = new System.Drawing.Size(90, 19);
+            this.RestaurantManagerCheckBox.TabIndex = 18;
+            this.RestaurantManagerCheckBox.Text = "مدیر رستوران";
+            this.RestaurantManagerCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(277, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 15);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "آدرس";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(277, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "پسورد";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(277, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "کد ملی";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "نام خانوادگی";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "نام";
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(256, 303);
+            this.EditButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(82, 22);
+            this.EditButton.TabIndex = 24;
+            this.EditButton.Text = "ویرایش";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(144, 303);
+            this.CancelButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(82, 22);
+            this.CancelButton.TabIndex = 25;
+            this.CancelButton.Text = "انصراف";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(32, 303);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(82, 22);
+            this.SaveButton.TabIndex = 26;
+            this.SaveButton.Text = "ذخیره";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // UserInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 360);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.AddressTetxtBox);
+            this.Controls.Add(this.PasswordTetxtBox);
+            this.Controls.Add(this.NationalCodeTetxtBox);
+            this.Controls.Add(this.LastNameTetxtBox);
+            this.Controls.Add(this.NameTetxtBox);
+            this.Controls.Add(this.RestaurantManagerCheckBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(390, 399);
+            this.MinimumSize = new System.Drawing.Size(390, 399);
+            this.Name = "UserInfoForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "اطلاعات کاربر";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.UserInfoForm_FormClosed);
+            this.Load += new System.EventHandler(this.UserInfoForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
 		#endregion
