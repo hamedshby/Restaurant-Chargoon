c346382f9ce534e4faac138eec39d6b7660b0ac5
Hadiii <h.mousavifakhr@gmail.com>
2023-06-12 12:27:56 +0330

    اضافه کردن حذف رستوران به ستون

--- RestaurantChargoon.UI.WinForm/Forms/Users/SigninUserFrom.Designer.cs
+++ RestaurantChargoon.UI.WinForm/Forms/Users/SigninUserFrom.Designer.cs
@@ -1,117 +1,112 @@
 ﻿namespace RestaurantChargoon.UI.WinForm.Forms
 {
-	partial class SigninUserFrom
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class SigninUserFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.NationalCodeTextBox = new System.Windows.Forms.TextBox();
-            this.PasswordTextBox = new System.Windows.Forms.TextBox();
-            this.Loginbtn = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            NationalCodeTextBox = new TextBox();
+            PasswordTextBox = new TextBox();
+            Loginbtn = new Button();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(250, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "کد ملی";
+            label1.AutoSize = true;
+            label1.Location = new Point(286, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 20);
+            label1.TabIndex = 0;
+            label1.Text = "کد ملی";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(250, 80);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "پسورد";
+            label2.AutoSize = true;
+            label2.Location = new Point(286, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 20);
+            label2.TabIndex = 1;
+            label2.Text = "پسورد";
             // 
             // NationalCodeTextBox
             // 
-            this.NationalCodeTextBox.Location = new System.Drawing.Point(82, 29);
-            this.NationalCodeTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.NationalCodeTextBox.Name = "NationalCodeTextBox";
-            this.NationalCodeTextBox.Size = new System.Drawing.Size(133, 23);
-            this.NationalCodeTextBox.TabIndex = 3;
-            this.NationalCodeTextBox.Text = "1111111111";
-            this.NationalCodeTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NationalCodeTextBox_KeyPress);
+            NationalCodeTextBox.Location = new Point(94, 39);
+            NationalCodeTextBox.Name = "NationalCodeTextBox";
+            NationalCodeTextBox.Size = new Size(151, 27);
+            NationalCodeTextBox.TabIndex = 3;
+            NationalCodeTextBox.Text = "9876543210";
+            NationalCodeTextBox.KeyPress += NationalCodeTextBox_KeyPress;
             // 
             // PasswordTextBox
             // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(82, 80);
-            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(133, 23);
-            this.PasswordTextBox.TabIndex = 4;
-            this.PasswordTextBox.Text = "111qqq";
-            this.PasswordTextBox.UseSystemPasswordChar = true;
+            PasswordTextBox.Location = new Point(94, 107);
+            PasswordTextBox.Name = "PasswordTextBox";
+            PasswordTextBox.Size = new Size(151, 27);
+            PasswordTextBox.TabIndex = 4;
+            PasswordTextBox.Text = "123ab";
+            PasswordTextBox.UseSystemPasswordChar = true;
             // 
             // Loginbtn
             // 
-            this.Loginbtn.Location = new System.Drawing.Point(82, 140);
-            this.Loginbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Loginbtn.Name = "Loginbtn";
-            this.Loginbtn.Size = new System.Drawing.Size(132, 26);
-            this.Loginbtn.TabIndex = 7;
-            this.Loginbtn.Text = "ورود";
-            this.Loginbtn.UseVisualStyleBackColor = true;
-            this.Loginbtn.Click += new System.EventHandler(this.enter_Click);
+            Loginbtn.Location = new Point(94, 187);
+            Loginbtn.Name = "Loginbtn";
+            Loginbtn.Size = new Size(151, 35);
+            Loginbtn.TabIndex = 7;
+            Loginbtn.Text = "ورود";
+            Loginbtn.UseVisualStyleBackColor = true;
+            Loginbtn.Click += enter_Click;
             // 
             // SigninUserFrom
             // 
-            this.AcceptButton = this.Loginbtn;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 196);
-            this.Controls.Add(this.Loginbtn);
-            this.Controls.Add(this.PasswordTextBox);
-            this.Controls.Add(this.NationalCodeTextBox);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "SigninUserFrom";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "فرم ورود کاربر";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SigninUserFrom_FormClosed);
-            this.Load += new System.EventHandler(this.SigninUserFrom_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AcceptButton = Loginbtn;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(371, 261);
+            Controls.Add(Loginbtn);
+            Controls.Add(PasswordTextBox);
+            Controls.Add(NationalCodeTextBox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "SigninUserFrom";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "فرم ورود کاربر";
+            FormClosed += SigninUserFrom_FormClosed;
+            Load += SigninUserFrom_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		}
+        #endregion
 
-		#endregion
-
-		private Label label1;
-		private Label label2;
-		private TextBox NationalCodeTextBox;
-		private TextBox PasswordTextBox;
-		private Button Loginbtn;
-	}
+        private Label label1;
+        private Label label2;
+        private TextBox NationalCodeTextBox;
+        private TextBox PasswordTextBox;
+        private Button Loginbtn;
+    }
 }
--- RestaurantChargoon.UI.WinForm/Forms/Users/SigninUserFrom.cs
+++ RestaurantChargoon.UI.WinForm/Forms/Users/SigninUserFrom.cs
@@ -6,59 +6,59 @@ using RestaurantChargoon.UI.WinForm.Services;
 
 namespace RestaurantChargoon.UI.WinForm.Forms
 {
-	public partial class SigninUserFrom : Form
-	{
-		private readonly UserService userService;
-		public SigninUserFrom()
-		{
-			InitializeComponent();
-			this.userService = new UserService();
-		}
+    public partial class SigninUserFrom : Form
+    {
+        private readonly UserService userService;
+        public SigninUserFrom()
+        {
+            InitializeComponent();
+            this.userService = new UserService();
+        }
 
-		#region Events
-		private void enter_Click(object sender, EventArgs e)
-		{
-			var user = userService.CheckUserPassword(NationalCodeTextBox.Text.Trim(), PasswordTextBox.Text.Trim());
-			if (user == null)
-			{
-				FormService.ShowErrorMessageBox("نام کاربری یا پسورد اشتباه هست");
-				return;
-			}
-			Program.userLogin = user;
-			if (user.UserType == UserType.RestaurantManager)
-			{
-				typeof(RestaurantDashboardForm).ShowDialog();
-			}
-			else if (user.UserType == UserType.User)
-			{
-				typeof(UserDashboardForm).ShowDialog();				
-			}
-		}
+        #region Events
+        private void enter_Click(object sender, EventArgs e)
+        {
+            var user = userService.CheckUserPassword(NationalCodeTextBox.Text.Trim(), PasswordTextBox.Text.Trim());
+            if (user == null)
+            {
+                FormService.ShowErrorMessageBox("نام کاربری یا پسورد اشتباه هست");
+                return;
+            }
+            Program.userLogin = user;
+            if (user.UserType == UserType.RestaurantManager)
+            {
+                typeof(RestaurantDashboardForm).ShowDialog();
+            }
+            else if (user.UserType == UserType.User)
+            {
+                typeof(UserDashboardForm).ShowDialog();
+            }
+        }
 
-		private void SigninUserFrom_FormClosed(object sender, FormClosedEventArgs e)
-		{
-			nameof(MainForm).ShowParentForm();
-		}
+        private void SigninUserFrom_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            nameof(MainForm).ShowParentForm();
+        }
 
-		private void SigninUserFrom_Load(object sender, EventArgs e)
-		{
-			nameof(MainForm).HideParentForm();
-		}
+        private void SigninUserFrom_Load(object sender, EventArgs e)
+        {
+            nameof(MainForm).HideParentForm();
+        }
 
         #endregion
 
         private void NationalCodeTextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
-			if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
-			{
-				e.Handled = true;
-			}
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
 
-			// only allow one decimal point
-			if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
-			{
-				e.Handled = true;
-			}
-		}
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
