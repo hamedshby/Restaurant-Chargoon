3a4123f91236f09b61de14f8f1a5c774400de845
hamedshby <hamedshby@gmail.com>
2023-06-16 21:45:14 +0330

    add files

---
+++ RestaurantChargoon.Infrastructure.EF/Repositories/FactorRepository/RestaurantRepository.cs
@@ -0,0 +1,10 @@
+﻿using RestaurantChargoon.Domain.Contracts;
+using RestaurantChargoon.Domain.DataModels;
+
+namespace RestaurantChargoon.Infrastructure.EF.Repositories
+{
+	public class RestaurantRepository : GenericRepository<Restaurant>, IRestaurantRepository
+	{
+
+	}
+}
---
+++ RestaurantChargoon.Infrastructure.EF/Repositories/FactorRepository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantChargoon.Domain.Contracts;
+using RestaurantChargoon.Domain.DataModels;
+using System.Linq;
+
+namespace RestaurantChargoon.Infrastructure.EF.Repositories
+{
+	public class UserRepository : GenericRepository<User>, IUserRepository
+	{
+		public User GetByNationalCode(string nationalCode)
+		{
+			var user = Get(c => c.NationalCode.Equals(nationalCode)).FirstOrDefault();
+			return user;
+		}
+
+		public User CheckUserPassword(string nationalCode, string password)
+		{
+			var user = Get(c => c.NationalCode.Equals(nationalCode) & c.Password.Equals(password)).FirstOrDefault();
+			return user;
+		}
+	}
+}
