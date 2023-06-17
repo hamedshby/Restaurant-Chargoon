c8b01d8b847a2c2dadec0d21884b13f8909a39ab
hamedshby <hamedshby@gmail.com>
2023-06-17 09:35:28 +0330

    ignore file

---
+++ RestaurantChargoon.Domain/Contracts/IDependencyRegistrar.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RestaurantChargoon.Domain.Contracts
+{
+	public interface IDependencyRegistrar
+	{
+		void ConfigureServices(ServiceCollection services);
+	}
+
+}
+
+
