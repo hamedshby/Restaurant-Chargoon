2948a0127280221b57a444d1eee8cd801c9da0fc
hamedshby <hamedshby@gmail.com>
2023-06-25 15:12:14 +0330

    edit dbcontext

---
+++ RestaurantChargoon.Infrastructure.EF/Repositories/FactorDetailRepository.cs
@@ -0,0 +1,16 @@
+﻿using RestaurantChargoon.Domain.Contracts;
+using RestaurantChargoon.Domain.DataModels;
+using System.Linq;
+
+namespace RestaurantChargoon.Infrastructure.EF.Repositories
+{
+    public class FactorDetailRepository : GenericRepository<FactorDetail>, IFactorDetailRepository
+    {
+        public decimal SumOfFactor(Factor factor)
+        {
+            var factorDetails = Get(c => c.FactorId == factor.Id).ToList();
+            var totalPrice = factorDetails.Sum(c => c.Price * c.Count);
+            return totalPrice;
+        }
+    }
+}
---
+++ RestaurantChargoon.Infrastructure.EF/Repositories/FoodRepository.cs
@@ -0,0 +1,10 @@
+﻿using RestaurantChargoon.Domain.Contracts;
+using RestaurantChargoon.Domain.DataModels;
+
+namespace RestaurantChargoon.Infrastructure.EF.Repositories
+{
+    public class FoodRepository : GenericRepository<Food>, IFoodRepository
+    {
+
+    }
+}
